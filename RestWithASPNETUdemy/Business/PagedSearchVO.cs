2d74eeea6192a8dce12e686b07618da4c7354211
Ricardo da Cunha <joaovitorleitesantos98@gmail.com>
2023-11-16 21:13:17 -0300

    Add project files.

---
+++ RestWithASPNETUdemy/Business/PagedSearchVO.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public class PagedSearchVO<T>
+    {
+        public int CurrentPage { get; internal set; }
+        public List<PersonVO> List { get; internal set; }
+        public int PageSize { get; internal set; }
+        public string SortDirections { get; internal set; }
+        public int TotalResults { get; internal set; }
+    }
+}
---
+++ RestWithASPNETUdemy/Hypermedia/HyperMediaLink.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace RestWithASPNETUdemy.Hypermedia
+{
+    public class HyperMediaLink
+    {
+        public string Rel {  get; set; }
+        private string href;  
+        public string Href
+        {
+            get
+            {
+                object _lock = new object();
+                lock (_lock)
+                {
+                    StringBuilder sb = new StringBuilder(href);
+                    return sb.Replace("%2f", "/").ToString();
+                }
+            }
+            set
+            {
+                href = value;
+            }
+        }
+        public string Type {  get; set; }    
+        public string Action {  get; set; }    
+    }
+}
